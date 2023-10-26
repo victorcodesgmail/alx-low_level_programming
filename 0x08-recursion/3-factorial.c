950a4b18e431be5a4cc5ff2768b51fccdd00e1d8
victorcodesgmail <victoryentertain97@gmail.com>
2023-10-25 20:34:52 -0700

    push

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,16 +15,15 @@ int factorial(int n)
 		return (-1);
 	}
 
-	if (n == 0)
+	if (n == 0 || n == 1)
 	{
 		return (1);
 	}
-	for (i = 1; i < n; i++)
+	for (i = 2 ; i <= n; i++)
 	{
-		f = i * n;
-		i++;
-		return (f);
+		f *=i;
 	}
-	return (0);
+
+	return (f);
 
 }
