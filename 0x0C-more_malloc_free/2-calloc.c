98d7786349253c28679b453a80af8e5be01fbc4b
victorcodesgmail <victoryentertain97@gmail.com>
2023-11-05 12:19:39 -0800

    push

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include  <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+    if (nmemb == 0 || size == 0)
+    {
+        return NULL;
+    }
+
+    ptr = malloc(nmemb * size);
+
+    if (ptr == NULL)
+    {
+        return NULL;
+    }
+
+    for ( i = 0; i < nmemb * size; i++)
+    {
+        ((char *)ptr)[i] = 0;
+    }
+
+    return ptr;
+}
