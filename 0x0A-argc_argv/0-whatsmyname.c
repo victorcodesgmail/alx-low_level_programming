cb1c5eb1f3c345d6a2e1549b4848bab47446498f
victorcodesgmail <victoryentertain97@gmail.com>
2023-11-04 22:16:15 -0700

    gitpush

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main-main
+ * @argc: no of arguments
+ * @argv: name of command and commandline arguments
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-        printf("%s\n", argv[argc - 1]);
-        return (0);
+printf("%s\n", argv[argc - 1]);
+return (0);
 }
