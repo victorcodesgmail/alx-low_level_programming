80fbdf784dabd9cdff33f45e3babb7a00b489f8a
victorcodesgmail <victoryentertain97@gmail.com>
2023-11-06 19:13:33 -0800

    description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ *struct dog-struct
+ *@name: n
+ *@age: age
+ *@owner: own
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 
 #endif
