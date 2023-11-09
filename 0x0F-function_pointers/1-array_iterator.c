d6188bed41b9e8b687bd22ea06a80c3d9122c621
victorcodesgmail <victoryentertain97@gmail.com>
2023-11-08 18:55:51 -0800

    push

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * array_iterator-main
+ * @array: array
+ * @size: s
+ * @action: func
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	 size_t i;
+
+	action(size);
+
+	for (i = 0;  i < size; i++)
+	{
+		action(array[i]);
+	}
+}
