965f9a765546cf3a777d6cf6d1caf224fa003058
victorcodesgmail <victoryentertain97@gmail.com>
2024-01-16 07:05:35 -0800

    strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+
+int countWords(char *str)
+{
+// Iterate through each character in the string
+// // Set flag to indicate that we are not in a word
+// // Increment the count when
+// // Set flag to indicate that we are in a word
+            }
+}
