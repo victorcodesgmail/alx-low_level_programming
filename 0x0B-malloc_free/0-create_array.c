14f6019a45137e90a0e00f595e3d5068f057a4e5
victorcodesgmail <victoryentertain97@gmail.com>
2024-01-16 07:12:56 -0800

    character allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,14 +18,14 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	array = malloc(size  * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
 	array[i] = c;
-	}
-	if (array == NULL)
-	{
-		return (NULL);
 	}
 return (array);
 
