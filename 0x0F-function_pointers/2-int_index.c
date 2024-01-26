8d489582b56161ff99555cf39005df0456912e39
victorcodesgmail <victoryentertain97@gmail.com>
2024-01-25 22:18:58 -0800

    search int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * int_index - fun
+ * @array: arra
+ * @size: size of int
+ * @cmd: 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for ( i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return -1;
+}
